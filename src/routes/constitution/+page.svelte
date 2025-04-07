ca73cb76ea7cc418e30c0a7291202a6f3fee8ad8
yamyambaklava <yvenstiamou@gmail.com>
2025-04-07 14:24:15 +0100

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -148,8 +148,6 @@
     
     <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Positional Breakdown</h3>
     
-    <p>28 Total Players</p>
-    
     <p class="underscore">Starters</p>
     <ul>
         <li>QB</li>
